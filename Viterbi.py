e14583331fb561ee75f2068dc6283f1472335cdf
mjaratli <maryamjaratli@hotmail.com>
2022-10-30 00:07:43 -0400

    Viterbi function seems to be working but numbers are off

--- Viterbi.py
+++ Viterbi.py
@@ -20,6 +20,12 @@ import sys
 #             prev_tags = []
 #             if col > 0:
 #                 prev_tags.append(tags_per_word[row][col])
+# # Initialization step
+# for index, element in enumerate(tags_per_word[0]):
+#     firstWord = tags_per_word[0][0]
+#     fa_element.append(firstWord)
+#     fa_element.append(tags_per_word[0][index])
+#     hi = 5
 class Word:
     def __init__(self, word, tag, score, backptr):
         self.word = word
@@ -146,10 +152,8 @@ def viterbi(bi_prob, lex_prob, line):
             if row == 0 and col > 0:
                 backptr = 0
                 tag = tags_per_word[row][col]
-                # l_prob = lex_prob.get([word, tag], 0)
-                # b_prob = bi_prob.get([tag, 'SOF'], 0)
                 try:
-                    score = math.log2(lex_prob[word, tag] + 1) * math.log2(bi_prob[tag, 'SOF'] + 1)
+                    score = math.log2(lex_prob[word, tag] + 1) + math.log2(bi_prob[tag, 'SOF'] + 1)
                 except KeyError:
                     score = 1
                 fa_element.append(tag)
@@ -160,15 +164,14 @@ def viterbi(bi_prob, lex_prob, line):
             # Iteration step
             if row > 0 and col > 0:
                 prev_tags = tags_per_word[row - 1][1:]
-                # Need to change this to a dictionary
-                # previous_score = final_array[row - 1][2]
+                # Dictionary of scores
                 previous_score = scores_dict[prev_tags[0], tags_per_word[row - 1][0]]
                 current_tag = tags_per_word[row][col]
                 try:
                     tagt_given_tagj = bi_prob[current_tag, prev_tags[0]]
                 except KeyError:
                     tagt_given_tagj = 1
-                max_score = math.log2(previous_score + 1) * math.log2(tagt_given_tagj + 1)
+                max_score = math.log2(previous_score + 1) + math.log2(tagt_given_tagj + 1)
                 backptr = prev_tags[0]
 
                 for prev_tag in prev_tags:
@@ -177,7 +180,7 @@ def viterbi(bi_prob, lex_prob, line):
                     except KeyError:
                         tagt_given_tagj = 1
                     previous_score = scores_dict[prev_tag, tags_per_word[row - 1][0]]
-                    max_score_compare = math.log2(previous_score + 1) * math.log2(tagt_given_tagj + 1)
+                    max_score_compare = math.log2(previous_score + 1) + math.log2(tagt_given_tagj + 1)
                     if max_score < max_score_compare:
                         max_score = max_score_compare
                         backptr = prev_tag
@@ -187,7 +190,7 @@ def viterbi(bi_prob, lex_prob, line):
                     prob_word_given_tag = math.log2(lex_prob[word, current_tag] + 1)
                 except KeyError:
                     prob_word_given_tag = 1
-                score = prob_word_given_tag * max_score
+                score = prob_word_given_tag + max_score
                 fa_element.append(score)
                 fa_element.append(backptr)
                 final_array.append(fa_element)
@@ -195,18 +198,31 @@ def viterbi(bi_prob, lex_prob, line):
 
             fa_element = fa_element[:1]
 
-    return 0
+    # Grabbing the elements for the tags
+    tested_tag_results = []
+    backpointer = final_array[-1][-1]
+    last_tag = final_array[-1][-3]
+    tested_tag_results.append(last_tag)
+    tested_tag_results.append(backpointer)
+    current_word = final_array[-1][-4]
+    # Sequence Identification
+    for row in reversed(range(len(final_array))):
+        if current_word != final_array[row - 1][0]:
+            if final_array[row - 1][1] == backpointer:
+                backpointer = final_array[row - 1][3]
+                if backpointer != 0:
+                    tested_tag_results.append(backpointer)
+                    current_word = final_array[row - 1][0]
+    # Reverse the results
+    tested_tag_results.reverse()
 
-    # # Initialization step
-    # for index, element in enumerate(tags_per_word[0]):
-    #     firstWord = tags_per_word[0][0]
-    #     fa_element.append(firstWord)
-    #     fa_element.append(tags_per_word[0][index])
-    #     hi = 5
+    # Returns the tag results
+    return tested_tag_results
 
 
 def process_test(bigram_p, lexical_p, fileName):
-    predictions = []
+    system_predictions = 0
+    total = 0
     # tagValues grabs the gold standard tags from each line while tagValuesGS is the gold standard tag values for the
     # entire test file
     tagValuesGS = []
@@ -224,11 +240,17 @@ def process_test(bigram_p, lexical_p, fileName):
                     word, tag = split_elements(element)
                     eachLine[index] = word
                     tagValues.append(tag)
-            tagValuesGS.append(tagValues)
+            # tagValuesGS.append(tagValues)
             # Run the viterbi algorithm on each line
-            viterbi(bigram_p, lexical_p, eachLine)
+            tested_results = viterbi(bigram_p, lexical_p, eachLine)
 
-    return predictions
+            # Get system predication counts and total counts
+            for index, element in enumerate(tested_results):
+                if element == tagValues[index]:
+                    system_predictions += 1
+                total += 1
+
+    return system_predictions, total
 
 
 # Call the text pre-processing function and return a processed list
@@ -246,4 +268,7 @@ word_tag_result, two_tags_result, tags_result = process_train(arg)
 # Calculate the bigram P(N given V) probabilities and the lexical P(a given DT) probabilities
 bigram_probabilities, lexical_probabilities = collect_prob(word_tag_result, two_tags_result, tags_result)
 # Run the Viterbi Algorithm
-predicted_tags = process_test(bigram_probabilities, lexical_probabilities, arg2)
+sys_predictions, total_tags = process_test(bigram_probabilities, lexical_probabilities, arg2)
+# Calculate the accuracy
+accuracy = (sys_predictions/total_tags) * 100
+hi = 5
